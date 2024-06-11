efcff07c6487d34b9c5da5ca7276c7412bd86e91
Suyog-Kulkarni <kulkarnisuyog192@gmail.com>
2024-06-11 17:18:51 +0530

    Add project files.

---
+++ NZWalks/NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(new string[] { "new", "name" });
+        }
+    }
+
+}
---
+++ NZWalks/NZWalks.API/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<NZWalksDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("NZWalksConnectionString"),
+        new MySqlServerVersion(new Version(8, 0, 36)) // Specify your MySQL server version here
+    )
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
